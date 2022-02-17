2f7ad6ff67e5f153f27dc608101d732845f06aed
Aego <aego.yu@gmail.com>
2022-02-18 03:22:32 +0800

    fixed root plugin bug

--- src/index.ts
+++ src/index.ts
@@ -22,24 +22,27 @@ export * from './types'
 export * from './utils'
 
 export const useIAM = (configs: IAMConfigs) => {
-  return usePlugin(async (instance, _, next) => {
-    await useDB({
-      databases: configs.databases,
-      models: [CaptchaModel, SessionModel, UserModel],
-    })
+  return usePlugin(
+    async (instance, _, next) => {
+      await useDB({
+        databases: configs.databases,
+        models: [CaptchaModel, SessionModel, UserModel],
+      })
 
-    useInstance('IAM.configs', configs)
+      useInstance('IAM.configs', configs)
 
-    useInstance('IAM.provider.auth', new AuthProvider(configs))
-    useInstance('IAM.provider.captcha', new CaptchaProvider(configs))
-    useInstance('IAM.provider.otp', new OTPProvider(configs))
-    useInstance('IAM.provider.rbac', new RBACProvider(configs))
-    useInstance('IAM.provider.session', new SessionProvider(configs))
-    useInstance('IAM.provider.user', new UserProvider(configs))
+      useInstance('IAM.provider.auth', new AuthProvider(configs))
+      useInstance('IAM.provider.captcha', new CaptchaProvider(configs))
+      useInstance('IAM.provider.otp', new OTPProvider(configs))
+      useInstance('IAM.provider.rbac', new RBACProvider(configs))
+      useInstance('IAM.provider.session', new SessionProvider(configs))
+      useInstance('IAM.provider.user', new UserProvider(configs))
 
-    instance.register(rbacPlugin)
-    instance.register(router.compile())
+      instance.register(rbacPlugin)
+      instance.register(router.compile())
 
-    next()
-  })
+      next()
+    },
+    { global: true },
+  )
 }
